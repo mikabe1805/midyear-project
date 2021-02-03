a65994217100fbfa638ee724b7ea3215d8cf54c8
Abi Pyatski <apyatski@gmail.com>
2021-02-03 12:17:58 -0500

    made come changes

--- pong_main.py
+++ pong_main.py
@@ -6,3 +6,20 @@ screen = turtle.Screen
 screen.title = "Chiaki Pong Showdown"
 screen.bgcolor = "pink"
 screen.setup(width = 2000, height = 800)
+
+#Paddles
+rp = turtle.Turtle()
+rp.speed(0)
+rp.shape("square")
+rp.color("purple")
+rp.shapesize(stretch_wid=6, stretch_len=2)
+rp.penup()
+rp.goto(400, 0)
+
+lp = turtle.Turtle()
+lp.speed(0)
+lp.shape("square")
+lp.color("purple")
+lp.shapesize(stretch_wid=6, stretch_len=2)
+lp.penup()
+lp.goto(-400, 0)
