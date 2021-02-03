fc8300af782e63d34a9b971ae23af94bb23a5d20
Abi Pyatski <apyatski@gmail.com>
2021-02-02 21:26:59 -0500

    made some changes

---
+++ pong_main.py
@@ -0,0 +1,8 @@
+import turtle
+
+"""Make a Chiaki picture the backround"""
+
+screen = turtle.Screen
+screen.title = "Chiaki Pong Showdown"
+screen.bgcolor = "pink"
+screen.setup(width = 2000, height = 800)
